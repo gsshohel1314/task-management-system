b4280b9f53ae707149d5fefe83bb413d26680e51
Shohel Rana <gsshohel1314@gmail.com>
2025-02-18 01:53:28 +0600

    Auth routes setup

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Container\Attributes\Auth;
+use App\Http\Controllers\API\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/register', 'register')->name('register');
+    Route::post('/login', 'login')->name('login');
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/user', function (Request $request) {
+        return response()->json($request->user());
+    });
+});
+
