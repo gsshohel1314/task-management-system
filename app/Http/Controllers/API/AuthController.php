fa340f1a0e77e2ba309eb2aaf76b369e18c7995d
Shohel Rana <gsshohel1314@gmail.com>
2025-02-18 03:34:56 +0600

    Authentication done

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -2,10 +2,57 @@
 
 namespace App\Http\Controllers\API;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\API\BaseController;
+use Illuminate\Support\Facades\Hash;
 
-class AuthController extends Controller
+class AuthController extends BaseController
 {
-    //
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|confirmed',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+        ]);
+
+        $success['token'] = $user->createToken('auth_token')->plainTextToken;
+        $success['token_type'] = 'Bearer';
+
+        return $this->sendResponse($success, 'User registered successfully.');
+    }
+
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|string',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $success['token'] = $user->createToken('auth_token')->plainTextToken;
+            $success['token_type'] = 'Bearer';
+
+            return $this->sendResponse($success, 'User logged in successfully.');
+        } else {
+            return $this->sendError('Invalid login details.', [], 401);
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return $this->sendResponse([], 'User logged out successfully.');
+    }
 }
